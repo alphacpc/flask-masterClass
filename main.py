5139f6dd5ed6ad1b0eab6964d09bc077b4418e25
alphacpc <alphacpc@gmail.com>
2022-04-10 13:03:28 +0000

    Family

--- main.py
+++ main.py
@@ -8,9 +8,8 @@ sfr.load_encoding_images('images/')
 
 # Load camera
 cap = cv2.VideoCapture(0)
-# cap.set(10, 200);
+cap.set(10, 400);
 frameParSeconde = cap.get(cv2.CAP_PROP_FPS);
-print(frameParSeconde)
 
 while True:
     success, frame = cap.read()
@@ -19,12 +18,27 @@ while True:
     face_locations, face_names = sfr.detect_known_faces(frame)
 
     for face_loc, name in zip(face_locations,face_names):
-        y1, x1, y2, x2= face_loc[0], face_loc[1], face_loc[2], face_loc[3]
 
-        cv2.putText(frame, name, (x2, y2 + 40), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 255,0), 2)
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255,0), 4)  
+        x1, y1, x2, y2= face_loc[3], face_loc[0], face_loc[1], face_loc[2]
 
-    cv2.imshow("Frame", frame)
+
+        if name == "alpha":
+            name = "kawou " + name
+        elif name == "mariama":
+            name = name + " diolat"
+        elif name == "hawa":
+            name = name + " lapin"
+        elif name == "Diamila":
+            name = name + " ndare"
+        elif name == "mouhamed":
+            name = name + " mbourou"
+
+        cv2.putText(frame, name, (x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 255,0), 1)
+        
+
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255,0), 2)  
+
+    cv2.imshow("Capture video", frame)
 
     key = cv2.waitKey(int(1000/frameParSeconde))
 
--- simple_face.py
+++ simple_face.py
@@ -50,14 +50,13 @@ class SimpleFacerec:
         for face_encoding in face_encodings:
             # See if the face is a match for the known face(s)
             matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
-            name = "Unknown"
+            name = "Inconnue"
 
             # Or instead, use the known face with the smallest distance to the new face
             face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
             best_match_index = np.argmin(face_distances)
             if matches[best_match_index]:
                 name = self.known_face_names[best_match_index]
-            print("je m'appelle ",name)
             face_names.append(name)
 
 

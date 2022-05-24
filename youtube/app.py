edbd0f52d7872ff024ff59a70e3a2d4b978dde29
alphacpc <alphacpc@gmail.com>
2022-05-24 16:41:35 +0000

    Download well but but the sound is missing for the videos

--- youtube/app.py
+++ youtube/app.py
@@ -1,31 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request
 from pytube import YouTube
 
-link = "https://www.youtube.com/watch?v=te2vKEjRUv0"
+# link = "https://youtu.be/Tor_gGi7e0g?list=PLTbQvx84FrASVK-B1S2z2fn16dRbLiNdr"
 
 app = Flask(__name__)
 
+# yt = None
 
-@app.route('/', methods=["POST"])
+@app.route('/', methods=["GET","POST"])
 def home():
+    if request.method == 'POST':
+        url = request.form.get('inputText')
+        yt = YouTube(url)
+        image = yt.thumbnail_url
+        videos = yt.streams.filter(file_extension='mp4')
 
-    # yt = YouTube(link)
+        print(yt,"k*****")
 
-    # print(yt)
-    # print("Title: ",yt.title)
-    # print("Number of views: ",yt.views)
-    # print("Length of video: ",yt.length,"seconds")
-    # print("Liste Streams : ",yt.streams)
-    # print("Les audios : ",yt.streams.filter(only_audio=True))
-    # print("Les video : ",yt.streams.filter(only_video=True))
+        url_test = url.split('?')[1]
 
-    # ys = yt.streams.get_by_itag('18')
-
-    # print("Downloading...")
-    # ys.download()
-    # print("Download completed!!")
+        return render_template('home.html', videos = videos, picture = image, title = yt.title, views = yt.views, url = url_test, yt = yt)
 
     return render_template('home.html')
+
+
+@app.route('/downloader/<itag>/<url>')
+def downloader(itag,url):
+
+    # print(yt)
+    print(itag)
+    yt = YouTube("https://www.youtube.com/watch?"+url)
+    ys = yt.streams.get_by_itag(itag)
+
+    print("Downloading...")
+    ys.download()
+    # print("Download completed!!")
+
+    return redirect('/')
 
 
 if __name__=='__main__':
